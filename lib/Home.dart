7adb17c931e88da777fe12333c105bb1e12fdab2
HKprogram <ricknogueira1231@gmail.com>
2021-03-09 16:27:05 -0300

    Menu de opções - PopupMenuButton

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:whatsapp/telas/AbaContatos.dart';
 import 'package:whatsapp/telas/AbaConversas.dart';
+import 'package:whatsapp/Login.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,12 +11,38 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController _tabController;
+  List<String> itensMenu = ["Configurações", "Deslogar"];
+
+  Future _recuperarDadosUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User usuarioLogado = await auth.currentUser;
+  }
 
   @override
   void initState() {
     super.initState();
-
+    _recuperarDadosUsuario();
     _tabController = TabController(length: 2, vsync: this);
+  }
+
+  _escolhaMenuItem(String itemEscolhido) {
+    switch(itemEscolhido){
+      case "Configurações":
+        print("Configurações");
+        break;
+      case "Deslogar":
+        _deslogarUsuario();
+        break;
+
+    }
+  }
+
+  _deslogarUsuario() async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
+
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
+
   }
 
   @override
@@ -30,6 +57,19 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           indicatorColor: Colors.white,
           tabs: <Widget>[Tab(text: "Conversas"), Tab(text: "Contatos")],
         ),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: _escolhaMenuItem,
+            itemBuilder: (context) {
+              return itensMenu.map((String item) {
+                return PopupMenuItem<String>(
+                  value: item,
+                  child: Text(item),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
       body: TabBarView(
         controller: _tabController,
