a418fa474f5e7af8018b293d593e6b051d7e73b6
HKprogram <ricknogueira1231@gmail.com>
2021-03-09 15:39:05 -0300

    Criando interface com abas

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:whatsapp/telas/AbaContatos.dart';
+import 'package:whatsapp/telas/AbaConversas.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("WhatsApp"),
+        bottom: TabBar(
+          indicatorWeight: 4,
+          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[Tab(text: "Conversas"), Tab(text: "Contatos")],
+        ),
       ),
-      body: Container(),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[AbaConversas(), AbaContatos()],
+      ),
     );
   }
 }
---
+++ lib/telas/AbaConversas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AbaConversas extends StatefulWidget {
+  @override
+  _AbaConversasState createState() => _AbaConversasState();
+}
+
+class _AbaConversasState extends State<AbaConversas> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Conversas"),
+    );;
+  }
+}
