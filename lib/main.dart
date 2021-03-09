95fe593ba6f97531babb4eda4e4daa142b36e212
HKprogram <55353527+soapmactavish23@users.noreply.github.com>
2021-03-09 00:42:44 -0300

    Configurando o projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'Home.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-  debugShowCheckedModeBanner: false,
-));
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.collection("usuario").doc("001").set({"nome":"Henrick"});
+
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
