4c4816e17e4f91bd84facfe67cc1399feae6a85f
HKprogram <ricknogueira1231@gmail.com>
2021-03-09 10:15:29 -0300

    interface de cadastro de usuário

--- lib/Cadastro.dart
+++ lib/Cadastro.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-import 'Cadastro.dart';
-
-class Login extends StatefulWidget {
+class Cadastro extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _CadastroState createState() => _CadastroState();
 }
 
-class _LoginState extends State<Login> {
+class _CadastroState extends State<Cadastro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro")
+      ),
       body: Container(
         decoration: BoxDecoration(color: Color(0xff075E54)),
         padding: EdgeInsets.all(16),
@@ -20,9 +21,9 @@ class _LoginState extends State<Login> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(bottom: 32),
+                    padding: EdgeInsets.only(bottom: 32),
                   child: Image.asset(
-                    "imagens/logo.png",
+                    "imagens/usuario.png",
                     width: 200,
                     height: 150,
                   ),
@@ -31,15 +32,33 @@ class _LoginState extends State<Login> {
                   padding: EdgeInsets.only(bottom: 8),
                   child: TextField(
                     autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Nome",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      )
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
                     keyboardType: TextInputType.emailAddress,
                     style: TextStyle(fontSize: 20),
                     decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "E-mail",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      )
+                    ),
                   ),
                 ),
                 TextField(
@@ -57,7 +76,7 @@ class _LoginState extends State<Login> {
                   padding: EdgeInsets.only(top: 16, bottom: 10),
                   child: RaisedButton(
                       child: Text(
-                        "Entrar",
+                        "Cadastrar",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                       color: Colors.green,
@@ -66,18 +85,6 @@ class _LoginState extends State<Login> {
                           borderRadius: BorderRadius.circular(32)),
                       onPressed: () {}),
                 ),
-                Center(
-                  child: GestureDetector(
-                    child: Text(
-                      "Não tem conta? cadastre-se!",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Cadastro()));
-                    },
-                  ),
-                )
               ],
             ),
           ),
