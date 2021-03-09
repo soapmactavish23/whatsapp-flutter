b8ff650e637582b76954337f264def6de4fd0204
HKprogram <ricknogueira1231@gmail.com>
2021-03-09 10:01:51 -0300

    Interace de Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'Login.dart';
 import 'Home.dart';
 
 void main() async{
 
-  WidgetsFlutterBinding.ensureInitialized();
+  /*WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseFirestore.instance.collection("usuario").doc("001").set({"nome":"Henrick"});
+  FirebaseFirestore.instance.collection("usuario").doc("002").set({"nome":"Amanda"});*/
 
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+      accentColor: Color(0xff25D366)
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
