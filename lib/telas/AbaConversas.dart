c79e3380655ac62a401268f98ea21dacefba3356
HKprogram <ricknogueira1231@gmail.com>
2021-03-11 11:54:09 -0300

    Listando conversas

--- lib/telas/AbaConversas.dart
+++ lib/telas/AbaConversas.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/model/Conversa.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AbaConversas extends StatefulWidget {
   @override
@@ -8,10 +11,14 @@ class AbaConversas extends StatefulWidget {
 
 class _AbaConversasState extends State<AbaConversas> {
   List<Conversa> _listaConversa = List();
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  String _idUsuarioLogado;
 
   @override
   void initState() {
     super.initState();
+    _recuperarDadosUsuario();
 
     Conversa conversa = new Conversa();
     conversa.nome = "Ana Clara";
@@ -22,29 +29,90 @@ class _AbaConversasState extends State<AbaConversas> {
     _listaConversa.add(conversa);
   }
 
+  _recuperarDadosUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User usuarioLogado = await auth.currentUser;
+    setState(() {
+      _idUsuarioLogado = usuarioLogado.uid;
+    });
+    _adicionarListenerConversas();
+  }
+
+  Stream<QuerySnapshot> _adicionarListenerConversas() {
+    final stream = db
+        .collection("conversas")
+        .doc(_idUsuarioLogado)
+        .collection("ultima_conversa").snapshots();
+
+    stream.listen((dados) {
+      setState(() {
+        _controller.add(dados);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _listaConversa.length,
-        itemBuilder: (context, indice) {
-          Conversa conversa = _listaConversa[indice];
+    return StreamBuilder<QuerySnapshot>(
+      stream: _controller.stream,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text("Carregando conversas..."),
+                  CircularProgressIndicator()
+                ],
+              ),
+            );
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Text("Erro ao carregar os dados!");
+            } else {
+              QuerySnapshot querySnapshot = snapshot.data;
+              if (querySnapshot.docs.length == 0) {
+                return Center(
+                  child: Text(
+                    "Você não tem nenhuma mensagem ainda",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                );
+              }
 
-          return ListTile(
-            contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-            leading: CircleAvatar(
-              maxRadius: 30,
-              backgroundColor: Colors.grey,
-              backgroundImage: NetworkImage(conversa.caminhoFoto),
-            ),
-            title: Text(
-              conversa.nome,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            subtitle: Text(
-              conversa.mensagem,
-              style: TextStyle(color: Colors.grey, fontSize: 14),
-            ),
-          );
-        });
+              return ListView.builder(
+                  itemCount: _listaConversa.length,
+                  itemBuilder: (context, indice) {
+
+                    //recupera mensagem
+                    List<DocumentSnapshot> conversas =
+                    querySnapshot.docs.toList();
+                    DocumentSnapshot item = conversas[indice];
+
+                    return ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                      leading: CircleAvatar(
+                        maxRadius: 30,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: NetworkImage(item["caminhoFoto"]),
+                      ),
+                      title: Text(
+                        item["nome"],
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      subtitle: Text(
+                        item["tipoMensagem"] == "texto" ? item["mensagem"]: "Imagem...",
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                    );
+                  });
+            }
+        }
+      },
+    );
   }
 }
